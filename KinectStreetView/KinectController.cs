1446f41071ba3a72a74d0d0c2c4e28efdf31a33f
KT <kasztnert@gmail.com>
2015-12-10 14:22:45 +0100

    Fénykép készítés megy, kinect mouse megy

---
+++ KinectStreetView/KinectController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+
+namespace KinectStreetView {
+	class KinectController {
+		static Body[] bodies = null;
+		static int ScreenWidth = Screen.PrimaryScreen.Bounds.Width;
+		static int ScreenHeight = Screen.PrimaryScreen.Bounds.Height;
+		static HandState lastHandState = HandState.Unknown;
+		static DateTime lastHandStateChange = DateTime.Now;
+		static bool mouseDownSent = false;
+		static bool goForwardSent = false;
+		static bool takePhotoSent = false;
+
+		public static event EventHandler GoForward;
+		public static event EventHandler TakePhoto;
+		/// <summary>
+		/// The max duration of a mouse click
+		/// </summary>
+		const int MouseClickInterval = 500;
+		public static void ProcessBodyFrame(BodyFrame frame) {
+			if (frame.BodyCount == 0) {
+				return;
+			}
+			bodies = new Body[frame.BodyCount];
+			frame.GetAndRefreshBodyData(bodies);
+			// only the person first engaged can control the app
+			var body = bodies.FirstOrDefault(b => b.IsTracked);
+			if (body == null) {
+				return;
+			}
+			var rhpos = body.Joints[JointType.HandRight].Position;
+			var lhpos = body.Joints[JointType.HandLeft].Position;
+			var splinebasepos = body.Joints[JointType.SpineBase].Position;
+			var headpos = body.Joints[JointType.Head].Position;
+			var vscreenHeight = headpos.Y - splinebasepos.Y;
+			var vscreenWidth = vscreenHeight;
+			// right hand raised, mouse control
+			if (rhpos.Z < splinebasepos.Z - 0.3 && lhpos.Z > splinebasepos.Z - 0.3) {
+				var cursX = rhpos.X;
+				if (cursX < splinebasepos.X - vscreenWidth / 2) {
+					cursX = splinebasepos.X - (float)vscreenWidth / 2;
+				} else if (cursX > splinebasepos.X + vscreenWidth / 2) {
+					cursX = splinebasepos.X + (float)vscreenWidth / 2;
+				}
+				cursX += (float)vscreenWidth / 2 - splinebasepos.X;
+				int x = (int)(ScreenWidth * (cursX / vscreenWidth));
+
+				var cursY = rhpos.Y;
+				if (cursY < splinebasepos.Y) {
+					cursY = splinebasepos.Y;
+				} else if (cursY > headpos.Y) {
+					cursY = headpos.Y;
+				}
+				cursY -= splinebasepos.Y;
+				int y = ScreenHeight - (int)(ScreenHeight * (cursY / vscreenHeight));
+				MouseControl.SetCursorPos(x, y);
+
+				if (lastHandState == HandState.Closed && body.HandRightState == HandState.Closed
+					&& (DateTime.Now - lastHandStateChange).Milliseconds > MouseClickInterval && !mouseDownSent) {
+
+					mouseDownSent = true;
+					MouseControl.MouseLeftDown();
+				} else if (body.HandRightState != lastHandState) {
+					switch (body.HandRightState) {
+					case HandState.Open:
+						if (lastHandState == HandState.Closed) {
+							if ((DateTime.Now - lastHandStateChange).Milliseconds < MouseClickInterval) {
+								MouseControl.DoMouseClick();
+							} else {
+								MouseControl.MouseLeftUp();
+							}
+						}
+						mouseDownSent = false;
+						break;
+					case HandState.Closed:
+						break;
+					case HandState.Lasso:
+					case HandState.Unknown:
+					case HandState.NotTracked:
+						if (mouseDownSent) {
+							MouseControl.MouseLeftUp();
+							mouseDownSent = false;
+						}
+						break;
+					default:
+						break;
+					}
+					lastHandState = body.HandRightState;
+					lastHandStateChange = DateTime.Now;
+				}
+			} else {
+				lastHandState = HandState.Unknown;
+			}
+			var lAnkle = body.Joints[JointType.AnkleLeft];
+			var rAnkle = body.Joints[JointType.AnkleRight];
+
+            if (lAnkle.TrackingState == TrackingState.Tracked
+				&& rAnkle.TrackingState == TrackingState.Tracked) {
+
+				if (Math.Abs(lAnkle.Position.Z - rAnkle.Position.Z) < vscreenHeight / 2) {
+					if (!goForwardSent) {
+						if (GoForward != null) {
+							GoForward(null, new EventArgs());
+						}
+						goForwardSent = true;
+					}
+				} else {
+					goForwardSent = false;
+				}
+			}
+
+			if (body.Joints[JointType.ElbowRight].Position.Y >= headpos.Y) {
+				if (!takePhotoSent && TakePhoto != null) {
+					TakePhoto(null, new EventArgs());
+				}
+				takePhotoSent = true;
+			} else {
+				takePhotoSent = false;
+			}
+		}
+	}
+}
--- KinectStreetView/MainWindow.xaml.cs
+++ KinectStreetView/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Forms;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Kinect;
 
 namespace KinectStreetView {
@@ -22,14 +16,22 @@ namespace KinectStreetView {
 		KinectSensor sensor = null;
 		MultiSourceFrameReader frameReader = null;
 		KinectBackgroundRemoval.BackgroundRemovalTool bgRemover = null;
+		int picWidth = 0, picHeight = 0;
+		WriteableBitmap bmp;
+
+		string path;
 		public MainWindow() {
 			InitializeComponent();
 			wbStreetView.Source = new Uri(System.IO.Path.GetFullPath("sv.html"));
-			imgForeground.Source = new BitmapImage(new Uri(System.IO.Path.GetFullPath("cili.png")));
+
 			sensor = KinectSensor.GetDefault();
 			frameReader = sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Body | FrameSourceTypes.Color | FrameSourceTypes.BodyIndex | FrameSourceTypes.Depth);
 			bgRemover = new KinectBackgroundRemoval.BackgroundRemovalTool(sensor.CoordinateMapper);
-		}
+			path = System.Configuration.ConfigurationManager.AppSettings["path"];
+			if (string.IsNullOrWhiteSpace(path)) {
+				path = System.IO.Path.GetTempPath();
+			}
+        }
 
 		private void btnGo_Click(object sender, RoutedEventArgs e) {
 			wbStreetView.InvokeScript("moveForward");
@@ -48,11 +50,27 @@ namespace KinectStreetView {
 		}
 
 		private void btnHideImage_Click(object sender, RoutedEventArgs e) {
-			wbStreetView.Visibility = wbStreetView.Visibility == Visibility.Hidden ? Visibility.Visible : Visibility.Hidden;
+			//wbStreetView.Visibility = wbStreetView.Visibility == Visibility.Hidden ? Visibility.Visible : Visibility.Hidden;
+
 		}
 
 		private void Window_Loaded(object sender, RoutedEventArgs e) {
 			frameReader.MultiSourceFrameArrived += FrameReader_MultiSourceFrameArrived;
+			sensor.Open();
+			pup.Height = wbStreetView.ActualHeight * 0.75;
+			pup.Width = wbStreetView.ActualWidth;
+			pup.VerticalOffset = 100;
+			pup.IsOpen = true;
+			KinectController.GoForward += KinectController_GoForward;
+			KinectController.TakePhoto += KinectController_TakePhoto;
+		}
+
+		private void KinectController_TakePhoto(object sender, EventArgs e) {
+			ScreenShot();
+		}
+
+		private void KinectController_GoForward(object sender, EventArgs e) {
+			wbStreetView.InvokeScript("moveForward");
 		}
 
 		private void FrameReader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e) {
@@ -60,11 +78,35 @@ namespace KinectStreetView {
 
 			using (var colorFrame = reference.ColorFrameReference.AcquireFrame())
 			using (var depthFrame = reference.DepthFrameReference.AcquireFrame())
-			using (var bodyIndexFrame = reference.BodyIndexFrameReference.AcquireFrame()) {
+			using (var bodyIndexFrame = reference.BodyIndexFrameReference.AcquireFrame())
+			using (var bodyFrame = reference.BodyFrameReference.AcquireFrame()) {
 				if (colorFrame != null && depthFrame != null && bodyIndexFrame != null) {
+					if (picWidth == 0 || picHeight == 0) {
+						picWidth = depthFrame.FrameDescription.Width;
+						picHeight = depthFrame.FrameDescription.Height;
+						bmp = new WriteableBitmap(picWidth, picHeight, 96, 96, PixelFormats.Bgra32, null);
+						imgForeground.Source = bmp;
+						imgForeground.Height = bmp.Height;
+						imgForeground.Width = bmp.Width;
+					}
 					var bytes = bgRemover.GreenScreen(colorFrame, depthFrame, bodyIndexFrame);
+					bmp.Lock();
+					Marshal.Copy(bytes, 0, bmp.BackBuffer, bytes.Length);
+					bmp.AddDirtyRect(new Int32Rect(0, 0, picWidth, picHeight));
+					bmp.Unlock();
+				}
+				if (bodyFrame != null) {
+					KinectController.ProcessBodyFrame(bodyFrame);
 				}
 			}
+		}
+
+		private void ScreenShot() {
+			var bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+										 Screen.PrimaryScreen.Bounds.Height);
+			Graphics graphics = Graphics.FromImage(bitmap);
+			graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+			bitmap.Save(System.IO.Path.Combine(path, DateTime.Now.ToString(@"yyyyMMdd_HHmmss.pn\g")), ImageFormat.Png);
 		}
 	}
 }
